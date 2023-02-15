953f1b341098d97b5c6689d3528d3025b9f17f3d
Kris <kodenow13@gmail.com>
2023-02-16 05:27:48 +0800

    gooey footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Gooey from './components/Gooey/Gooey';
 function App() {
   return (
     <div className="App">
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Gooey/>
     </div>
   );
 }
